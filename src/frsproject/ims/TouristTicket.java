4f4212cd251493957f85ea57b54d6399cd08f200
kishore <kishorekannan63@gmail.com>
2021-03-04 20:21:48 +0530

    checkpoint3

--- src/frsproject/ims/TouristTicket.java
+++ src/frsproject/ims/TouristTicket.java
@@ -1,20 +1,23 @@
 package frsproject.ims;
 import java.time.LocalDateTime;
 public class TouristTicket {
-    String pnr;
-    String from;
-    String to;
-    String departureDateTime;
-    String arrivalDateTime;
-    String seatNo;
-    float price;
-    boolean cancelled;
-    String hotelAddress;
-    String[] selectedTouristLocation;
+    private String pnr;
+    private String from;
+    private String to;
+    private String departureDateTime;
+    private String arrivalDateTime;
+    private String seatNo;
+    private float price;
+    private boolean cancelled;
+    private String hotelAddress;
+    private String[] selectedTouristLocation;
+    private Flight flight;
+    private Passenger passenger;
 
-    TouristTicket(String pnr, String from, String to, String departureDateTime,
-                  String arrivalDateTime, String seatNo, float price,
-                  boolean cancelled, String hotelAddress, String[] selectedTouristLocation) {
+    public TouristTicket(String pnr, String from, String to, String departureDateTime,
+                         String arrivalDateTime, String seatNo, float price,
+                         boolean cancelled, String hotelAddress, String[] selectedTouristLocation,
+                         Flight flight, Passenger passenger) {
         this.pnr = pnr;
         this.from = from;
         this.to = to;
@@ -25,34 +28,126 @@ public class TouristTicket {
         this.cancelled = cancelled;
         this.hotelAddress = hotelAddress;
         this.selectedTouristLocation = selectedTouristLocation;
+        this.flight = flight;
+        this.passenger = passenger;
     }
 
+    public String getPnr() {
+        return pnr;
+    }
 
+    public void setPnr(String pnr) {
+        this.pnr = pnr;
+    }
 
-    String checkStatus() {
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getDepartureDateTime() {
+        return departureDateTime;
+    }
+
+    public void setDepartureDateTime(String departureDateTime) {
+        this.departureDateTime = departureDateTime;
+    }
+
+    public String getArrivalDateTime() {
+        return arrivalDateTime;
+    }
+
+    public void setArrivalDateTime(String arrivalDateTime) {
+        this.arrivalDateTime = arrivalDateTime;
+    }
+
+    public String getSeatNo() {
+        return seatNo;
+    }
+
+    public void setSeatNo(String seatNo) {
+        this.seatNo = seatNo;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    public void setHotelAddress(String hotelAddress) {
+        this.hotelAddress = hotelAddress;
+    }
+
+    public String[] getSelectedTouristLocation() {
+        return selectedTouristLocation;
+    }
+
+    public void setSelectedTouristLocation(String[] selectedTouristLocation) {
+        this.selectedTouristLocation = selectedTouristLocation;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(Passenger passenger) {
+        this.passenger = passenger;
+    }
+
+    public String checkStatus() {
         return cancelled ? "Cancelled" : "Confirmed";
     }
 
-    int getFlightDuration() {
+    public int getFlightDuration() {
         LocalDateTime departureLocalDateTime = LocalDateTime.parse(departureDateTime);
         LocalDateTime arrivalLocalDateTime = LocalDateTime.parse(arrivalDateTime);
         return (arrivalLocalDateTime.getDayOfMonth() - departureLocalDateTime.getDayOfMonth()) * 24 +
                 (arrivalLocalDateTime.getHour() - departureLocalDateTime.getHour());
     }
 
-    void cancel() {
+    public void cancel() {
         cancelled = true;
     }
 
-    String getHotelAddress() {
+    public String getHotelAddress() {
         return hotelAddress;
     }
 
-    String[] getTouristLocation() {
+    public String[] getTouristLocation() {
         return selectedTouristLocation;
     }
 
-    void removeTouristLocation(String location) {
+    public void removeTouristLocation(String location) {
         int index=-1;
         for (int i=0; i<selectedTouristLocation.length; i++) {
             if (selectedTouristLocation[i].equals(location)) {
@@ -69,7 +164,7 @@ public class TouristTicket {
         selectedTouristLocation[selectedTouristLocation.length-1] = null;
     }
 
-    void addTouristLocation(String location) {
+    public void addTouristLocation(String location) {
         int index = -1;
         for (int i=0; i< selectedTouristLocation.length; i++) {
             if (selectedTouristLocation[i] == null) {
